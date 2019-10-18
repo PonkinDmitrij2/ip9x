8b576836375d212936c301368eebf8d199d78bfa
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-10-18 15:19:24 +0300

    Adds iteration task 3 while

--- exercises/iteration/3-while.js
+++ exercises/iteration/3-while.js
@@ -1,9 +1,18 @@
 'use strict';
 
+// Use while loop and accumulator variable
+// to calculate sum of all given arguments
+// For example sum(1, 2, 3) should return 6
 const sum = (...args) => {
-  // Use while loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let result = 0;
+  let i = 0;
+
+  while (i < args.length) {
+    result += args[i];
+    i++;
+  }
+
+  return result;
 };
 
 module.exports = { sum };
