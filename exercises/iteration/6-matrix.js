9981d44dc4263879cd439a8af13425f265caf4cd
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-10-18 15:57:54 +0300

    Adds iteration task 6 matrix

--- exercises/iteration/6-matrix.js
+++ exercises/iteration/6-matrix.js
@@ -1,9 +1,23 @@
 'use strict';
-
+// Use nested for loop to find max value in 2d matrix
+// For example max([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+// should return 9
 const max = matrix => {
-  // Use nested for loop to find max value in 2d matrix
-  // For example max([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-  // should return 9
+  let maxValue = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    const array = matrix[i];
+
+    for (let j = 0; j < array.length; j++) {
+      const item = array[j];
+
+      if (item > maxValue) {
+        maxValue = item;
+      }
+    }
+  }
+
+  return maxValue;
 };
 
 module.exports = { max };
