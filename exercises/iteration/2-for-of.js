78af2c08d1ebfb148437c6b0753b9c2555521b26
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-10-18 15:02:37 +0300

    Adds iteration task 2 for-of

--- exercises/iteration/2-for-of.js
+++ exercises/iteration/2-for-of.js
@@ -1,9 +1,16 @@
 'use strict';
 
+// Use for..of loop and accumulator variable
+// to calculate sum of all given arguments
+// For example sum(1, 2, 3) should return 6
 const sum = (...args) => {
-  // Use for..of loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let result = 0;
+
+  for (const num of args) {
+    result += num;
+  }
+
+  return result;
 };
 
 module.exports = { sum };
