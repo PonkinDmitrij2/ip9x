b20328f7f757948252f57685a3c625574bed0b03
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-10-18 15:21:58 +0300

    Adds iteration task 4 do while

--- exercises/iteration/4-do-while.js
+++ exercises/iteration/4-do-while.js
@@ -1,9 +1,18 @@
 'use strict';
 
+// Use do..while loop and accumulator variable
+// to calculate sum of all given arguments
+// For example sum(1, 2, 3) should return 6
 const sum = (...args) => {
-  // Use do..while loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let result = 0;
+  let i = 0;
+
+  do {
+    result += args[i];
+    i++;
+  } while (i < args.length);
+
+  return result;
 };
 
 module.exports = { sum };
