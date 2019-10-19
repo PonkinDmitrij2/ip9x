672ae1ea01c7af53e59561820541b7fea2c2fe5e
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-10-19 16:51:35 +0300

    Adds iteration task 7 ages

--- exercises/iteration/7-ages.js
+++ exercises/iteration/7-ages.js
@@ -1,19 +1,25 @@
-'use strict';
-
+'use strict';  // Use for..in to calculate age for each person
+// For example ages({
+//   lenin: { born: 1870, died: 1924 },
+//   mao: { born: 1893, died: 1976 },
+//   gandhi: { born: 1869, died: 1948 },
+//   hirohito: { born: 1901, died: 1989 },
+// })
+// should return {
+//   lenin: 54,
+//   mao: 83,
+//   gandhi: 79,
+//   hirohito: 88,
+// }
 const ages = persons => {
-  // Use for..in to calculate age for each person
-  // For example ages({
-  //   lenin: { born: 1870, died: 1924 },
-  //   mao: { born: 1893, died: 1976 },
-  //   gandhi: { born: 1869, died: 1948 },
-  //   hirohito: { born: 1901, died: 1989 },
-  // })
-  // should return {
-  //   lenin: 54,
-  //   mao: 83,
-  //   gandhi: 79,
-  //   hirohito: 88,
-  // }
+  const result = {};
+
+  for (const name in persons) {
+    const { born, died } = persons[name];
+    result[name] = died - born;
+  }
+
+  return result;
 };
 
 module.exports = { ages };
